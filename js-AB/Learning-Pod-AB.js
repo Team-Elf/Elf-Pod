3bbc359f7e9563e4a1b108c7f3c6a67a3eb117cc
Alexander Bonawitz <Alexander.L.Bonawitz@gmail.com>
2019-12-11 09:23:27 -0600

    Testing merging with Gaston

--- js-AB/Learning-Pod-AB.js
+++ js-AB/Learning-Pod-AB.js
@@ -3,4 +3,6 @@
 // 1. Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
 var x = [1, 2, 3, 8];
 
-function minMax(x)
+function minMax(x) {
+    return
+}
