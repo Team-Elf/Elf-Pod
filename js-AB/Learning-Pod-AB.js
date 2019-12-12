771e67076bf49e2dc9de1b8179825f6c6b39f658
Alexander Bonawitz <Alexander.L.Bonawitz@gmail.com>
2019-12-12 09:27:50 -0600

    set-up and tested the very basic (if even) slot machine. it really is just an input checker that returns true or false based on if all input in a .log are matching

--- js-AB/Learning-Pod-AB.js
+++ js-AB/Learning-Pod-AB.js
@@ -19,7 +19,21 @@ function calculator(num1,operator,num2) {
         //subtraction setup
         '*': num1 * num2,
         //multiplication
-        '/': num2 ? num1 / num2 : 'Cannot divide by 0!'
+        '/': num2 ? num1 / num2 : 'Imagine trying to divide by 0...'
     }[operator]
 }
-// console.log(calculator(1, "+", 3));
+// console.log(calculator(1, "*", 3));
+
+
+// 3. Create a function that takes in an array (slot machine outcome) and returns true if all elements in the array are identical, and false otherwise. The array will contain 4 elements.
+// var result;
+function testJackpot(result) {
+    if(result[0]==result[1]&&result[1]==result[2]&&result[2]==result[3]){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+// console.log(testJackpot([2, 2, 2, 2]));
