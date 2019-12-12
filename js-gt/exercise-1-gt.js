b605694beb78477684273413c365de69c3a349cc
Gaston Tarango <gaston.tarango@gmail.com>
2019-12-12 10:35:42 -0600

    finished 3 exercises

--- js-gt/exercise-1-gt.js
+++ js-gt/exercise-1-gt.js
@@ -1,5 +1,7 @@
 "use strict";
 alert("1")
+
+// https://edabit.com/challenge/i6YqzHcSiPiEQKjeX
 // Given an array of scrabble tiles, create a function that outputs the maximum possible score a player can achieve by summing up the total number of points for all the tiles in their hand. Each hand contains 7 scrabble tiles.
 
 //     Here's an example hand:
@@ -37,3 +39,16 @@ alert("1")
 // ]) ➞ 15
 // Notes
 // Here, each tile is represented as an object with two keys: tile and score.
+
+function maximumScore(tileHand) {
+    var x = 0;
+    for(var i = 0; i < tileHand.length; i++) {
+        x += tileHand[i].score;
+    }
+    return x;
+}
+
+console.log((maximumScore([{tile: 'N', score: 1}, {tile: 'K', score: 5},  {tile: 'Z', score: 10}, {tile: 'X', score: 8}, {tile: 'D', score: 2}, {tile: 'A', score: 1}, {tile: 'E', score: 1}]), 28));
+console.log((maximumScore([{tile: 'B', score: 2}, {tile: 'V', score: 4},  {tile: 'F', score: 4}, {tile: 'U', score: 1}, {tile: 'D', score: 2}, {tile: 'O', score: 1}, {tile: 'U', score: 1}]), 15));
+console.log((maximumScore([{tile: 'M', score: 3}, {tile: 'Q', score: 10},  {tile: 'O', score: 1}, {tile: 'E', score: 1}, {tile: 'E', score: 1}, {tile: 'I', score: 1}, {tile: 'A', score: 1}]), 18));
+console.log((maximumScore([{tile: 'A', score: 1}, {tile: 'E', score: 1},  {tile: 'O', score: 1}, {tile: 'E', score: 1}, {tile: 'E', score: 1}, {tile: 'I', score: 1}, {tile: 'A', score: 1}]), 7));
--- js-gt/exercise-3-gt.js
+++ js-gt/exercise-3-gt.js
@@ -1,2 +1,35 @@
 "use strict";
 alert("3");
+
+// https://edabit.com/challenge/XdAR3KohR5w7rjrFg
+// Create a function that takes in an array of numbers and returns the sum of its cubes.
+//
+//     Examples
+// sumOfCubes([1, 5, 9]) ➞ 855
+// // Since 1^3 + 5^3 + 9^3 = 1 + 125 + 729 = 855
+//
+// sumOfCubes([3, 4, 5]) ➞ 216
+//
+// sumOfCubes([2]) ➞ 8
+//
+// sumOfCubes([]) ➞ 0
+// Notes
+// If given an empty array, return 0.
+
+function sumOfCubes(nums) {
+    var i, sum = 0;
+    for (i in nums) {
+        sum += nums[i] * nums[i] * nums[i];
+    }
+    return sum
+}
+
+console.log((sumOfCubes([1, 5, 9]), 855));
+console.log((sumOfCubes([3, 4, 5]), 216));
+console.log((sumOfCubes([1, 1, 1]), 3));
+console.log((sumOfCubes([2]), 8));
+console.log((sumOfCubes([5, 1, 2]), 134));
+console.log((sumOfCubes([32]), 32768));
+console.log((sumOfCubes([5, 9, 4, 4, 9]), 1711));
+console.log((sumOfCubes([0, 1, 2]), 9));
+console.log((sumOfCubes([]), 0));
