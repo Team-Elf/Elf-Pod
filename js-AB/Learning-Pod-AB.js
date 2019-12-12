7705db2187ef1e2d809b249a0022e07eea599db7
Alexander Bonawitz <Alexander.L.Bonawitz@gmail.com>
2019-12-12 11:02:41 -0600

    Fixed some more stuff and added links to problems, also fixed some problems up

--- js-AB/Learning-Pod-AB.js
+++ js-AB/Learning-Pod-AB.js
@@ -1,16 +1,16 @@
 "use strict";
 
 // 1. Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
-var x = [1, 2, 3, 8];
-
-function minMax(x) {
-
-    return Math.min.apply(Math, x);
-    return Math.max.apply(Math, x);
-}
+// var x = [1, 2, 3, 8];
+//
+// function minMax(x) {
+//
+//     return Math.min.apply(Math, x);
+//     return Math.max.apply(Math, x);
+// }
 // console.log(minMax(x));
 
-
+//link: https://edabit.com/challenge/AtoWYYC9THAH5HbS2
 // 2. Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
 
 function calculator(num1,operator,num2) {
@@ -20,13 +20,16 @@ function calculator(num1,operator,num2) {
         '-': num1 - num2,
         //subtraction setup
         '*': num1 * num2,
-        //multiplication
+        //multiplication setup
         '/': num2 ? num1 / num2 : 'Imagine trying to divide by 0...'
     }[operator]
 }
 // console.log(calculator(1, "*", 3));
+// console.log(calculator(4, "/", 2));
+// console.log(calculator(8, "/", 0));
+// console.log(calculator(2, "+", 2));
 
-
+//link: https://edabit.com/challenge/hxyvTffvdT4E238CY
 // 3. Create a function that takes in an array (slot machine outcome) and returns true if all elements in the array are identical, and false otherwise. The array will contain 4 elements.
 // var result;
 function testJackpot(result) {
@@ -40,15 +43,19 @@ function testJackpot(result) {
 
 // console.log(testJackpot([2, 2, 2, 2]));
 
+// Link: https://edabit.com/challenge/ew9dry9RzoaeiTzwX
 // 4. Write a function that inserts a white space between every instance of a lower character followed immediately by an upper character.
 
 function insertWhitespace(s) {
     return s.split("").map((i, index)=>{
         //map is a storage command, where it holds on to a key and returns it as stored
-        if(index!==0)
-            if(i !== i.toLowerCase())
-                return " "+i;
-        return i;
+        if(index!== 0) {
+            if (i !== i.toLowerCase()) {
+                return " " + i;
+            }
+            return i;
+        }
     }).join("");
 }
-// console.log(insertWhitespace("TheQuickBrownFoxJumpedOverTheLazyCoder"));
+// console.log(insertWhitespace(" TheQuickBrownFoxJumpedOverTheLazyCoder"));
+// console.log(insertWhitespace(" SallySellsSeaShellsAtTheSeaShore"));
