35d1d7eb514f9136aece80a31cbcea2ccddf7d77
Chelseahaggray20 <chelsea.haggray20@gmail.com>
2019-12-20 15:07:19 -0600

    removing duplicates, first exercise CH

--- js-CH/exercise 1.js
+++ js-CH/exercise 1.js
@@ -2,13 +2,21 @@
 
 //Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
 
-function removeDups {
 
+
+
+function removeDups(arr) {
+    var names = ['Chelsea', 'Adra', 'Alex', 'Kim', 'Megan', 'Alex', 'Monique', 'Caysen', 'Brycen', 'Kim'];
+    let unique = {};
+    names.forEach(function(i) {
+        if(!unique[i]) {
+            unique[i] = true;
+        }
+    });
+    return Object.keys(unique);
 }
 
-
-
-
+console.log(removeDups((['Chelsea', 'Adra', 'Alex', 'Kim', 'Megan', 'Alex', 'Monique', 'Caysen', 'Brycen', 'Kim'])));
 
 
 
